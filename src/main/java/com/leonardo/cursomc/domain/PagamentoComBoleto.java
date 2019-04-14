0107d626cd3ff79ac62c20c17261f897247979bf
Leonardo <leonardo.hrjesus@hotmail.com>
2019-04-14 14:02:54 -0300

    INSERINDO UMA NOVA CATEGORIA COM POST / PRIMEIRO COMMIT

---
+++ src/main/java/com/leonardo/cursomc/domain/PagamentoComBoleto.java
@@ -0,0 +1,47 @@
+package com.leonardo.cursomc.domain;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.leonardo.cursomc.domain.enums.EstadoPagamento;
+
+@Entity
+public class PagamentoComBoleto  extends Pagamento{
+	private static final long serialVersionUID = 1L;
+
+	@JsonFormat(pattern="dd/MM/yyyy")
+	private Date dataVencimento;
+	@JsonFormat(pattern="dd/MM/yyyy")
+	private Date dataPagamento;
+	
+	public PagamentoComBoleto() {
+		
+	}
+
+	public PagamentoComBoleto(Integer id, EstadoPagamento estadoPagamento, Pedido pedido,Date dataVencimento,Date dataPagamento) {
+		super(id, estadoPagamento, pedido);
+		this.dataPagamento = dataPagamento;
+		this.dataVencimento = dataVencimento;
+	}
+
+	public Date getDataVencimento() {
+		return dataVencimento;
+	}
+
+	public void setDataVencimento(Date dataVencimento) {
+		this.dataVencimento = dataVencimento;
+	}
+
+	public Date getDataPagamento() {
+		return dataPagamento;
+	}
+
+	public void setDataPagamento(Date dataPagamento) {
+		this.dataPagamento = dataPagamento;
+	}
+	
+	
+	
+}
---
+++ src/main/java/com/leonardo/cursomc/services/CategoriaService.java
@@ -0,0 +1,23 @@
+package com.leonardo.cursomc.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.leonardo.cursomc.domain.Categoria;
+import com.leonardo.cursomc.repositories.CategoriaRepository;
+import com.leonardo.cursomc.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class CategoriaService {
+	
+	@Autowired
+	private CategoriaRepository repo;
+	
+	public Categoria find(Integer id) {
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+		}
+}
